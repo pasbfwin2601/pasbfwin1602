d1607c579b2da7ebdd99f70f69a874eae27c4399
Pablo Sayanees <juan.sayanes@estudiantes.utec.edu.uy>
2021-02-16 19:19:09 -0300

    registro usuarios y medicos

--- app/src/main/java/com/bit/creciendojuntos/RegisterActivity.java
+++ app/src/main/java/com/bit/creciendojuntos/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.bit.creciendojuntos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,7 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.bit.creciendojuntos.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,8 +37,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mPref = getApplicationContext().getSharedPreferences("typeUser",MODE_PRIVATE);
-        String selectedUser = mPref.getString("user","");
-        Toast.makeText(RegisterActivity.this, "El valor seleccionado fue "+ selectedUser, Toast.LENGTH_SHORT).show();
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
@@ -44,6 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         mTextInputEmail = (TextInputEditText) findViewById(R.id.textInputEmail);
         mTextInputPassword = (TextInputEditText) findViewById(R.id.textInputPassword);
         mButtonRegister = (Button) findViewById(R.id.btnRegister);
+
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -61,7 +65,36 @@ public class RegisterActivity extends AppCompatActivity {
         final String email = mTextInputEmail.getText().toString();
         final String password = mTextInputPassword.getText().toString();
 
+        if (!nombre.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+            if (password.length() >= 6) {
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            saveUser(nombre,email);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(RegisterActivity.this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Debe ingresar todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private void saveUser(String nombre, String email) {
+        String selectedUser = mPref.getString("user","");
+        User user = new User();
+        user.setEmail(email);
+        user.setNombre(nombre);
 
+        if (selectedUser.equals("medico")){
+            mDatabase.child("Users").child("Medico").push().setValue(user);
+        } else if (selectedUser.equals("usuario")){
+            mDatabase.child("Users").child("Usuario").push().setValue(user);
+        }
     }
 }
