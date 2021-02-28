f38cb210568db1fc8af919b4dda4ac47058e6f3f
pasbfwin2601 <pasbfwin2601@gmail.com>
2021-02-28 18:39:28 -0300

    le agregue comentarios

--- app/src/main/java/com/bit/creciendojuntos/activities/LoginActivity.java
+++ app/src/main/java/com/bit/creciendojuntos/activities/LoginActivity.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,8 +23,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import dmax.dialog.SpotsDialog;
 
@@ -33,12 +40,12 @@ public class LoginActivity extends AppCompatActivity {
     TextInputEditText mTextInputPassword;
     Button mButtonLogin;
     SharedPreferences mPref;
-
-    // Alerta dialog
     AlertDialog mDialog;
+    AlertDialog mDialogUsuario;
+    AlertDialog mDialogMedico;
 
     FirebaseAuth mAuth;
-    DatabaseReference mDatabase;
+    public DatabaseReference mDatabase;
 
 
     @Override
@@ -56,6 +63,8 @@ public class LoginActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mDialog = new SpotsDialog.Builder().setContext(LoginActivity.this).setMessage("Espere un momento...").build();
+        mDialogUsuario = new SpotsDialog.Builder().setContext(LoginActivity.this).setMessage("Usuario NO registrado...").build();
+        mDialogMedico = new SpotsDialog.Builder().setContext(LoginActivity.this).setMessage("Medico NO registrado...").build();
         mPref = getApplicationContext().getSharedPreferences("typeUser",MODE_PRIVATE);
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,28 +78,71 @@ public class LoginActivity extends AppCompatActivity {
         String email = mTextInputEmail.getText().toString();
         String password = mTextInputPassword.getText().toString();
 
-        //Verifica que los campos no esten vacios
+
         if (!email.isEmpty() && !password.isEmpty()) {
             if (password.length() >= 6) {
-                //Alerta Dialog en login
+
                 mDialog.show();
-                 mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             String user = mPref.getString("user","");
                             if (user.equals("usuario")) {
-                                Intent intent = new Intent(LoginActivity.this, PantallaUsuarioActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
-                            } else {
-                                Intent intent = new Intent(LoginActivity.this, PantallaMedicoActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
+                                mDatabase.child("Users").child("usuario").child(mAuth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.exists()){
+                                            Intent intent = new Intent(LoginActivity.this, PantallaUsuarioActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(intent);
+                                        } else {
+                                            mDialogUsuario.show();
+                                            new Timer().schedule(new TimerTask() {
+                                                public void run() {
+                                                    mDialogUsuario.dismiss();
+                                                }}, 2000);
+                                            mAuth.signOut();
+                                        }
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
                             }
-                        }
+                            else {
+                                mDatabase.child("Users").child("medico").child(mAuth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.exists()) {
+                                            Intent intent = new Intent(LoginActivity.this, PantallaMedicoActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(intent);
+                                        } else {
+                                            mDialogMedico.show();
+                                            new Timer().schedule(new TimerTask() {
+                                                public void run() {
+                                                    mDialogMedico.dismiss();
+                                                }}, 2000);
+                                            mAuth.signOut();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                                }
+
+                            }
                         else {
-                            Toast.makeText(LoginActivity.this, "La contraseña o el password son incorrectos", Toast.LENGTH_SHORT).show();
+                              Toast.makeText(LoginActivity.this, "La contraseña o el password son incorrectos", Toast.LENGTH_SHORT).show();
                         }
                         mDialog.dismiss();
                     }
